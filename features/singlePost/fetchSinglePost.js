fae8445892df749d939ee8005b66f2fc359f71e9
Babul Akter <babulakterfsd@gmail.com>
2022-10-23 11:04:12 +0600

    both fetching implemented

--- features/singlePost/fetchSinglePost.js
+++ features/singlePost/fetchSinglePost.js
@@ -1,9 +1,11 @@
 const {createAsyncThunk} = require('@reduxjs/toolkit')
 const fetch = require('node-fetch')
+const fetchSimiliarPosts = require('../similiarPosts/fetchSimiliarPosts')
 
-const fetchSinglePost = createAsyncThunk('fetch/singlePost', async() => {
+const fetchSinglePost = createAsyncThunk('fetch/singlePost', async(_, {dispatch}) => {
     const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=1')
     const post = await response.json()
+    dispatch(fetchSimiliarPosts())
     return [post, post.title]
 })
 
--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const fetchSinglePost = require('./features/singlePost/fetchSinglePost')
 const fetchSimiliarPosts = require('./features/similiarPosts/fetchSimiliarPosts')
 
 store.subscribe(() => {
-    console.log(store.getState().singlePost.singlePost);
+    console.log(store.getState().singlePost.singlePost.title);
     console.log(store.getState().similiarPosts.similiarPosts);
 })
 
 store.dispatch(fetchSinglePost())
-store.dispatch(fetchSimiliarPosts())
